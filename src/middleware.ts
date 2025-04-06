df12ae7a459996e6d271054ff2fc398a07dcb2ca
Andrew Clark <cap@mail.com>
2025-04-06 02:39:49 +0200

    Add accounts page

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { isAuthenticated } from "./app/_utils/amplifyServerUtil";
 
 export async function middleware(request: NextRequest) {
-  
+
   const authenticated = await isAuthenticated();
-  
+
   // Redirect based on authentication state
   if (!authenticated && request.nextUrl.pathname !== '/signin') {
     return NextResponse.redirect(new URL('/signin', request.url));
@@ -16,5 +16,5 @@ export async function middleware(request: NextRequest) {
 
 // Apply middleware to all routes except API routes (optional)
 export const config = {
-  matcher: ['/', '/dashboard:path*'], // Applies to all pages
+  matcher: ['/', '/dashboard:path*', '/accounts:path*'], // Applies to all pages
 };
